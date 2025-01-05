5c555487bb108eaad78ce25a23b1cc03fc5c6c29
v1d3rm3 <victorschinaider@outlook.com>
2025-01-04 23:09:57 -0300

    Inicializa o projeto exemplo1 com configuração do Gradle e estrutura básica do Spring Boot

---
+++ src/test/java/br/ufrn/testes/exemplo1/Exemplo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufrn.testes.exemplo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exemplo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
